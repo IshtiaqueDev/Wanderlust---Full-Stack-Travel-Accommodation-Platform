6dd797f9654ff789cd417eefaf2ab670427d202b
IshtiaqueDev <javedjanijaved810@gmail.com>
2026-06-03 21:32:22 -0700

    Updated Code

--- Schema.js
+++ Schema.js
@@ -1,14 +1,12 @@
 const joi=require("joi");
 
 const listingSchema=joi.object({
-    listing: joi.object({
         title:joi.string().required(),
         description:joi.string().required(),
         location:joi.string().required(),
         country:joi.string().required(),
         price:joi.number().required().min(0),
         img:joi.string().allow("",null) 
-    }).required()
 })
 
     module.exports={listingSchema};
