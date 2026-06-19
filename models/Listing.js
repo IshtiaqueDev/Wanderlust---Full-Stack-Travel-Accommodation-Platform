7abea0be2bf11f828c8e0d39b8cd444827094c19
IshtiaqueDev <javedjanijaved810@gmail.com>
2026-06-18 23:57:37 -0700

    Added Delete Review Functionality

--- models/Listing.js
+++ models/Listing.js
@@ -1,4 +1,5 @@
 const mongoose=require("mongoose");
+const Review=require("./review.js");
 main().then((res)=>{
     console.log("Connected with Database Successfully!");
 }).catch((err)=>{
@@ -30,6 +31,12 @@ const listingSchema=new mongoose.Schema({
         ref:"Review"
     }]
 })
+
+
+listingSchema.post("findOneAndDelete",async(listing)=>{
+    if(listing){
+    await Review.deleteMany({_id:{$in:listing.reviews}});
+}});
 
 const Listing=mongoose.model("Listing",listingSchema);
     module.exports=Listing;
