13aea1cea3952cb1eb2f5d5718e5cc8fd31063a2
IshtiaqueDev <javedjanijaved810@gmail.com>
2026-07-23 08:21:08 +0500

    Added Listings

--- init/index.js
+++ init/index.js
@@ -2,6 +2,10 @@ const mongoose=require("mongoose");
 const AllData=require("../init/data");
 const Listing=require("../models/Listing");
 
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+
 main().then(()=>{
     console.log("Connnection Formed Successfully!");
 }).catch((err)=>{
@@ -10,13 +14,13 @@ main().then(()=>{
 
 
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+    await mongoose.connect("mongodb+srv://ishtiaquealiai12_db_user:oMjPi6kR0zr8QiK6@cluster0.rxdkbki.mongodb.net");
 }
 
 
 const initDB=async()=>{
-    await Listing.deleteMany({ owner: "6a4dbfdd38bdb6b6d3fe25a8"});
-    AllData.data=AllData.data.map((obj)=>({...obj,owner:"6a4dbfdd38bdb6b6d3fe25a8"}));
+    await Listing.deleteMany({ owner: "6a5dd3982790b5bd7b41723a"});
+    AllData.data=AllData.data.map((obj)=>({...obj,owner:"6a5dd3982790b5bd7b41723a"}));
    await  Listing.insertMany(AllData.data);
 }
 
